8bf9c1498f5faa969c05fea91c93e0c1d570c2e1
David Nitzsche <davidnitzsche@Nitzsches-MacBook-Pro.local>
2011-07-27 19:27:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dental::Application.config.secret_token = '06423e2a7546e730cd230779e33586ce7aa909cbb591a102909831350d1dbab6bfad094dc3e8a3bae29e39145149e5fd58c1159c4cef4961dc43ae426dd8b0f9'
